24220025826917022ce11988df4d5a505791d80f
Volodymyr Isyk <volodka.isyk.95@gmail.com>
2023-01-17 02:08:46 +0200

    added others function

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ let arr = ["+", "-", "*", "/"]
 let monitor = document.querySelector("#monitor");
 let numbers = document.querySelector("#numbers");
 let res = document.querySelector('.res')
+
 numbers.addEventListener("click", show)
 
 function show(e) {
@@ -13,61 +14,125 @@ function show(e) {
     res.textContent += elem;
 
     if (elem == "C") {
-        monitor.value = "";
-        res.textContent = "";
-        first, second, oper = null;
+        resetValue()
     }
-    // if (elem == "=") {
-        if (first && second && oper) {
-            switch (oper) {
-                case '*': 
-                    monitor.value = first * second;
-                    showResInInput(first, second, oper, monitor.value)
-                    break;
-                case '/': 
-                    monitor.value = first / second;
-                    showResInInput(first, second, oper, monitor.value)
-                    break;
-                case '-': 
-                    monitor.value = first - second;
-                    showResInInput(first, second, oper, monitor.value)
-                    break;
-                case '+': 
-                    monitor.value = Number(first) + Number(second);
-                    showResInInput(first, second, oper, monitor.value)
-                    break;
-            }
-            isEnter = true;
-        }
-    // }
 
-    if (isNaN(elem)) {
-        if (arr.indexOf(elem) != -1) {
-            if (!isEnter) {
-                if (first && !second) {
-                    oper = elem;
-                    monitor.value = '';                    
-                }
-            } else {
-                oper = elem;
-                first = monitor.value;
-                second = null;
-                monitor.value = ''
-            }
+    if (first && second && oper) {
+        if (elem == "=") {
+            createResult(first, oper, second);
+            animation()
         }
+        else if (arr.indexOf(elem) != -1){
+            createOtherResult(first, oper, second);
+        }
+        isEnter = true;
+    }
+    if (isNaN(elem)) {
+
+    if (arr.indexOf(elem) != -1) {
+        if (!isEnter) {
+            if (first && !second) {
+                oper = elem;
+                monitor.value = '';                    
+            }
+        } else {
+            oper = elem;
+            first = monitor.value;
+            second = null;
+            monitor.value = ''
+        }
+    }
     } else {
         monitor.value += elem;
         
         if (!second && !oper) {
             first = monitor.value;
-
         }
         if (first && oper) {
             second = monitor.value;
         }
     }
+
+    function resetValue() {
+        monitor.value = null;
+        res.textContent = '';
+        first = null;
+        second = null;
+    }
+    
+    function createResult(first, operator, second) {
+        switch (operator) {
+            case '*': 
+                monitor.value = first * second;
+                showResInInput(first, second, operator, monitor.value)
+                return monitor.value
+            case '/': 
+                monitor.value = first / second;
+                showResInInput(first, second, operator, monitor.value)
+                return monitor.value
+            case '-': 
+                monitor.value = first - second;
+                showResInInput(first, second, operator, monitor.value)
+                return monitor.value
+            case '+': 
+                monitor.value = Number(first) + Number(second);
+                showResInInput(first, second, operator, monitor.value)
+                return monitor.value
+        }
+        
+    }
+
+    function createOtherResult(first, operator, second) {
+        switch (operator) {
+            case '*': 
+                monitor.value = first * second;
+                second = monitor.value;
+                ShowCreateOtherResultInInput()
+                animation()
+                
+
+                return monitor.value
+            case '/': 
+                monitor.value = first / second;
+                second = monitor.value;
+                ShowCreateOtherResultInInput()
+                animation()
+
+                return monitor.value
+            case '-': 
+                monitor.value = first - second;
+                second = monitor.value;
+                ShowCreateOtherResultInInput()
+                animation()
+
+                return monitor.value
+            case '+': 
+                monitor.value = Number(first) + Number(second);
+                second = monitor.value;
+                ShowCreateOtherResultInInput()
+                animation()
+
+                if (operator == 'C') {
+                    resetValue()
+                }
+                return monitor.value
+        }
+    }
+
     function showResInInput(value1, value2, oper, sum) {
-        inputTextValue = `${value1} ${oper} ${value2} = ${sum}`;
+        inputTextValue = `${value1} ${oper} ${value2} = ${sum} `;
         return res.textContent = inputTextValue;
     }
+    function ShowCreateOtherResultInInput() {
+        return res.textContent = monitor.value + ' ' + elem + ' ';
+    }
+}
+
+function animation() {
+    res.style.background = '#bab6b6'
+    setTimeout(()=> {
+        
+        res.style.background = '#fff'
+    }, 150)
+    setTimeout
 }
